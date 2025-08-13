261866a558325854c612d603a6c3ce08b1032cc8
steve.addams <steve.addams@BK-STEVEADDAMS>
2025-08-13 23:04:47 +0300

    Demo

---
+++ ADV03/ConditonFunc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV03
+{
+    internal class ConditonFunc
+    {
+        public static bool IsOdd(int num)
+        {
+            return num % 2 != 0;
+        }
+        public static bool IsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+
+        public static bool NamesLen(string name)
+        {
+            return name.Length > 4;
+
+        }
+        public static bool NamesLenEqual3(string name)
+        {
+            return name.Length == 3;
+
+        }
+
+    }
+}
+
---
+++ ADV03/StringFunctions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV03
+{
+    internal class StringFunctions
+    {
+        public static int UpperCaseCount(string s)
+        {
+            if (string.IsNullOrEmpty(s)) { return 0; }
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int LowerCaseCount(string s)
+        {
+            if (string.IsNullOrEmpty(s)) { return 0; }
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLower(s[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
+
